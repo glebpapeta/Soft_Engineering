166f0bce090c8a8949871120bcfe5dad0fc2c2e5
glebpapeta <gleb.papeta05@gmail.com>
2025-09-29 03:47:59 +0500

    Create sam 3_5.py

---
+++ sam 3_5.py
@@ -0,0 +1,12 @@
+string = 'hello'
+string = 'hello'
+memory = 'world'
+counter = 0
+values = [0, 2, 4, 6, 8, 10]
+while counter != 10:
+    if counter in values:
+        print(string + ' ' + memory)
+    else:
+        print(string)
+    counter += 1
+print(string + ' world')
