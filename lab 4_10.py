58170288dd7d645f9166af57ca6d727874c49a9e
glebpapeta <gleb.papeta05@gmail.com>
2025-10-06 02:15:09 +0500

    Create lab 4_10.py

---
+++ lab 4_10.py
@@ -0,0 +1,13 @@
+global result
+
+def rectangle():
+    a = float(input("Ширина: "))
+    b = float(input("Высота: "))
+    global result
+    result = a * b
+
+def triangle():
+    a = float(input("Основание: "))
+    h = float(input("Высота: "))
+    global result
+    result = 0.5 * a * h
