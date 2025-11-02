0ea09ca9438fae79fe79b446de6858af4649287c
glebpapeta <gleb.papeta05@gmail.com>
2025-11-03 03:05:17 +0500

    Update lab_1.py

--- lab_1.py
+++ lab_1.py
@@ -3,7 +3,7 @@ class Car:  # Определение класса 'Car'
         self.make = make  # Присваивание значения параметра 'make' атрибуту 'make' объекта
         self.model = model  # Присваивание значения параметра 'model' атрибуту 'model' объекта
 
-# Создание экземпляра класса 'Car' с параметрами 'Chevrolet' и 'Lacetti'
-my_car = Car("Chevrolet", "Lacetti")
+# Создание экземпляра класса 'Car' с параметрами 'Toyota' и 'Corolla'
+my_car = Car("Toyota", "Corolla")
 # Вывод информации об автомобиле на экран
 print(f"Бренд: {my_car.make}, Модель: {my_car.model}")
