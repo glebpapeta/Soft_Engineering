557a7678db22718b5d1153267de7dc39150ee962
glebpapeta <gleb.papeta05@gmail.com>
2025-11-03 03:19:21 +0500

    Update lab_3.py

--- lab_3.py
+++ lab_3.py
@@ -1,4 +1,20 @@
-input_string = 'HelloWorld'
-result = tuple(input_string)
-print(result)
-print(list(result))
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+        
+    def drive(self):
+        print(f"Driving the {self.make} {self.model}")
+
+class ElectricCar(Car):
+    def __init__(self, make, model, battery_capacity):
+        super().__init__(make, model)
+        self.battery_capacity = battery_capacity
+    
+    def charge(self):
+        print(f"Charging the {self.make} {self.model} with {self.battery_capacity} kWh")
+
+
+my_electric_car = ElectricCar("Tesla", "Model S", 75)
+my_electric_car.drive()
+my_electric_car.charge()
